52a9f0abd7f01a947be1f416e55162ded14a2453
Liquor4k993 <Kiratuber01@gmail.com>
2025-11-08 22:30:43 +0300

    Задача 2

--- src/main/java/liquor4k/Main.java
+++ src/main/java/liquor4k/Main.java
@@ -2,14 +2,41 @@ package liquor4k;
 
 public class Main {
     public static void main(String[] args) {
-        //Задача 1
+        //задача 1
         int[] integerArray = new int[3];
         integerArray[0] = 1;
         integerArray[1] = 2;
         integerArray[2] = 3;
 
         double[] doubleArray = {1.57, 7.654, 9.986};
-
         String[] stringArray = {"Apple", "Banana", "Orange"};
+
+        // Задача 2
+        for (int i = 0; i < integerArray.length; i++) {
+            if (i == integerArray.length - 1) {
+                System.out.print(integerArray[i]);
+            } else {
+                System.out.print(integerArray[i] + ", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < doubleArray.length; i++) {
+            if (i == doubleArray.length - 1) {
+                System.out.print(doubleArray[i]);
+            } else {
+                System.out.print(doubleArray[i] + ", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < stringArray.length; i++) {
+            if (i == stringArray.length - 1) {
+                System.out.print(stringArray[i]);
+            } else {
+                System.out.print(stringArray[i] + ", ");
+            }
+        }
+        System.out.println();
     }
 }
