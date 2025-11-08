a082f9c945eb3df3d3466ce01b0bec23bc028a06
Liquor4k993 <Kiratuber01@gmail.com>
2025-11-08 22:28:46 +0300

    Задача 1

--- src/main/java/liquor4k/Main.java
+++ src/main/java/liquor4k/Main.java
@@ -1,1 +1,15 @@
 package liquor4k;
+
+public class Main {
+    public static void main(String[] args) {
+        //Задача 1
+        int[] integerArray = new int[3];
+        integerArray[0] = 1;
+        integerArray[1] = 2;
+        integerArray[2] = 3;
+
+        double[] doubleArray = {1.57, 7.654, 9.986};
+
+        String[] stringArray = {"Apple", "Banana", "Orange"};
+    }
+}
